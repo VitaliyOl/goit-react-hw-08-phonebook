0bc77d5b56dd117995339a8d19f287ff3b4b79d7
VitaliyOl <114258498+VitaliyOl@users.noreply.github.com>
2023-06-19 18:40:41 +0000

    Deploying to gh-pages from @ VitaliyOl/goit-react-hw-08-phonebook@af45a6a25e66723dfb60c17ec8a2e6f375860005 🚀

---
+++ static/js/415.d7b934e5.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[415],{5415:function(e,t,r){r.r(t);var n=r(3329);t.default=function(){return(0,n.jsx)("div",{style:{width:360,textAlign:"center",marginTop:100},children:(0,n.jsx)("h1",{children:"WELCOME TO YOUR PHONEBOOK"})})}}}]);
+//# sourceMappingURL=415.d7b934e5.chunk.js.map
